4f4b315d5df9c0b2be7b1fca9466cc3a2f61acc0
Tony <aanthon@umich.edu>
2024-11-14 10:42:07 -0500

    v1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,11 @@
 // Code to implement the gcf function goes here
+int gcf(int a, int b) {
+    int q, r;
+    q = a / b;
+    r = a % b;
+    if (r != 0) {
+        gcf (b, r);
+    } else if (r == 0) {
+        return b;
+    }
+}
